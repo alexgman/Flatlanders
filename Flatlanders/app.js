2e0908fec883293f915acdf61ae307e4e93df18a
= <aleksandr.gordon@gmail.com>
2016-07-05 00:24:32 -0500

    AngularTutorial from http://courseware.codeschool.com/shaping-up-with-angular-js/Slides/level01-05.pdf

---
+++ Flatlanders/app.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("store", []);
+
+    app.controller("StoreController", function () {
+        this.products = gem;
+    });
+
+    var gem = [
+    {
+        name: "Dodecahedron",
+        price: 2.95,
+        description: ". . .",
+        canPurchase: true,
+        soldOut: false,
+        images: [
+            {
+                full: 'dodecahedron-01-full.jpg',
+                thumb: 'dodecahedron-01-thumb.jpg'
+            },
+            {
+                full: "dodecahedron-02-full.jpg"
+            }
+        ]
+    },
+    {
+        name: "Pentagonal Gem",
+        price: 5.95,
+        description: ". . .",
+        canPurchase: false,
+        images:
+            [
+                {
+                    full: 'dodecahedron-01-full.jpg',
+                    thumb: 'dodecahedron-01-thumb.jpg'
+                },
+                {
+                    full: "dodecahedron-02-full.jpg"
+                }
+            ]
+    }
+    ];
+})();
